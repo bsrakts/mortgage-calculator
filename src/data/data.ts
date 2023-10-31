fb3b1568d64cc9fd90d7a54b2cb6e010ad4498da
Büşra Aktaş <diba.aktas@ogr.sakarya.edu.tr>
2023-10-31 23:41:27 +0300

    created project

---
+++ src/data/data.ts
@@ -0,0 +1,7 @@
+const inputFields = [
+    { label: 'Kredi Miktarı ($)', id: 'loanAmount' },
+    { label: 'Yıllık Faiz Oranı (%)', id: 'interestRate' },
+    { label: 'Kredi Vadesi (yıl)', id: 'loanTerm' },
+  ];
+  
+  export default inputFields;
